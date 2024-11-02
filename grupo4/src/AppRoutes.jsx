44ddbcc34c71cfcf94f785369d66a57140076137
João Moreira <jgmoreira1809@gmail.com>
2024-11-01 21:51:57 -0300

    Initial commit

---
+++ grupo4/src/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Routers, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login/Login";
+
+export default function AppRoutes() {
+  return (
+    <Routers>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </Routers>
+  );
+}
---
+++ grupo4/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Seja bem-vindo(a) ao nosso site!</p>
+    </div>
+  );
+}
