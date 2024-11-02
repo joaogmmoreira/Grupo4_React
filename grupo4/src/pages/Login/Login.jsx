aaadd614058454d855edac698ff258c5697fb608
cintra444 <cintra.eber@gmail.com>
2024-11-02 18:06:43 -0300

    iniciando login

--- grupo4/src/pages/Login/Login.jsx
+++ grupo4/src/pages/Login/Login.jsx
@@ -1,8 +1,22 @@
 export default function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <div class='container'>
+      <div><img src="../../assets/react.svg" alt="logo" /></div>
+      <div>
+        <h1>Entre com seu usuário e senha</h1>
+      </div>
+      <div>
+        <form>
+          <label>Username:</label>
+          <input type="text" name="username" />
+        </form>
+      </div>
       <p>Entre com seu usuário e senha</p>
+
+      </div>
+      
+
     </>
   );
 }
