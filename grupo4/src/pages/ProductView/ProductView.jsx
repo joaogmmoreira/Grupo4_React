4ff7c9640374953e915a5445ccf53a5f29483409
weliton schitini <schitini1987@gmail.com>
2024-11-04 11:26:54 -0300

    inicio da pagina ProductView

--- grupo4/src/pages/ProductView/ProductView.jsx
+++ grupo4/src/pages/ProductView/ProductView.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProductById } from "../../api/Api";
-
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+import "./ProductView.css"
 export default function ProductView() {
   const [product, setProduct] = useState(null);
   const endPoint = useParams().id;
@@ -14,9 +16,15 @@ export default function ProductView() {
   const renderPage = () => {
     return product ? (
       <>
+      <Header/>
+      <div className="Style.container">
         <h1>{product.nome}</h1>
         <h1>{product.descrição}</h1>
         <img src={product.imgurl}></img>
+        <button>Adicionar ao carrinho</button>
+      </div>
+       
+      <Footer/>
       </>
     ) : (
       <h1>Carregando...</h1>
