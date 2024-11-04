26497fcc88bd14acac78262e6ae41c68cc7993b1
João Moreira <jgmoreira1809@gmail.com>
2024-11-04 12:55:22 -0300

    Update ProductView.jsx

--- grupo4/src/pages/ProductView/ProductView.jsx
+++ grupo4/src/pages/ProductView/ProductView.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProductById } from "../../api/Api";
-import Header from "../../components/Header/Header"
-import Footer from "../../components/Footer/Footer"
-import "./ProductView.css"
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import "./ProductView.css";
+
 export default function ProductView() {
   const [product, setProduct] = useState(null);
   const endPoint = useParams().id;
@@ -13,18 +14,15 @@ export default function ProductView() {
     setProduct(newProduct);
   };
 
-  const renderPage = () => {
+  const renderProductView = () => {
     return product ? (
       <>
-      <Header/>
-      <div className="Style.container">
-        <h1>{product.nome}</h1>
-        <h1>{product.descrição}</h1>
-        <img src={product.imgurl}></img>
-        <button>Adicionar ao carrinho</button>
-      </div>
-       
-      <Footer/>
+        <div className="Style.container">
+          <h1>{product.nome}</h1>
+          <h1>{product.descrição}</h1>
+          <img src={product.imgurl}></img>
+          <button>Adicionar ao carrinho</button>
+        </div>
       </>
     ) : (
       <h1>Carregando...</h1>
@@ -35,5 +33,11 @@ export default function ProductView() {
     fetchProductById();
   }, []);
 
-  return renderPage();
+  return (
+    <>
+      <Header />
+      {renderProductView()}
+      <Footer />
+    </>
+  );
 }
